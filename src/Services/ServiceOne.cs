8ab2c8c1fe04204c4a72ba783d5cb2cdce5c7e04
Joao Paulo Pozo Lindgren <jplindgrenbr@hotmail.com>
2014-07-17 22:46:20 -0300

    loading project

---
+++ src/Services/ServiceOne.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Services {
+    public class ServiceOne {
+        public bool DoSomething() {
+            return true;
+        }
+    }// class
+}
